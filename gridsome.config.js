0505430fdac6f6ad830248b460cd34bea7f8c011
6110110628 <6110110628@psu.ac.th>
2021-07-23 00:16:11 +0700

    edit link

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,9 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Get To Know',
+  siteUrl: 'https://6110110628.github.io',
+  pathPrefix: '/intro-lee',
+  plugins: [],
   
 }
